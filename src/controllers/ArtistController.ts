3dc39123915ae9b498916794c7105c3188eff9d1
unknown <mkoltonowski@edu.cdv.pl>
2022-07-21 12:15:26 +0200

    Refactored artist controller

--- src/controllers/ArtistController.ts
+++ src/controllers/ArtistController.ts
@@ -12,11 +12,7 @@ import sequelize from "../helpers/databaseHelper";
 
 class ArtistController{
 
-    /**
-     * [get] All artist list
-     * @param {Request} request
-     * @param {Response} response
-     */
+
     async getAllArtist(request: Request, response: Response){
         try{
             const artists = await Artist.findAll();
@@ -28,11 +24,6 @@ class ArtistController{
         }
     }
 
-    /**
-     * [GET] Filtered by genre artist list
-     * @param {Request<{},{},{},Filter>} request
-     * @param {Response} response
-     */
     async listByGenreId(request: Request<{},{},{},Filter>, response: Response){
         const {query} = request;
         const filter: Filter = {
@@ -67,43 +58,39 @@ class ArtistController{
     }
 
 
-
-    /**
-     * [GET] The count of music tracks of an artist
-     * @param {Request<{},{},{},Filter>} request
-     * @param {Response} response
-     */
     countMusicTracks(request: Request<{},{},{},Filter>, response: Response){
+        
         const {query} = request;
-        if(query.artistId){
-                /**DB QUERY*/
-                const result = Track.count({
-                    where:{
-                        AlbumId: {
-                            [Op.in]: sequelize.literal(`(SELECT AlbumId FROM albums WHERE ArtistId = ${query.artistId??0})`)
-                        }
-                    }
-                })
-
-                const artist = Artist.findByPk(query.artistId);
-
-                Promise.all([artist, result]).then(values=>{
-
-                    response.send({
-                        "Artist": values[0],
-                        "TrackCount": values[1]
-                    })
-
-                }).catch(error=>{
-                    response.status(500).send(`Internal server error: ${error}`)
-                    console.log('Error: ',error);
-                })
-
-
+        
+        if(!query.artist_id){
+            response.status(400).send('Error: artist_id is not specified')
+            return;
         }
-        else{
-            response.status(400).send('Error: ArtistId is not specified')
-        }
+
+        const result = Track.findAll({
+            attributes:[[sequelize.literal('COUNT(TrackId)'), 'result']],
+            replacements:{artist_id: query.artist_id},
+            where:{
+                AlbumId: {
+                    [Op.in]: sequelize.literal(`(SELECT AlbumId FROM albums WHERE ArtistId = :artist_id)`)
+                }
+            }
+        })
+
+        const artist = Artist.findByPk(query.artist_id);
+
+        Promise.all([artist, result]).then(values => {
+            const [artist, result] = values;
+            response.send({
+                "artist": artist,
+                "track_count": result
+            })
+
+        }).catch(error=>{
+            response.status(500).send(`Internal server error: ${error}`)
+            console.log('Error: ',error);
+        })
+
     }
 }
 
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export interface Filter{
     [key: string]:number | undefined,
     genreId?: number
-    artistId?: number
+    artist_id?: number
 }
